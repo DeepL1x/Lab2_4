8541e05233718110b15ac2979b5ae92a56daf674
Victor <biliavskyi.viktor@lll.kpi.ua>
2021-05-01 03:35:44 +0300

    added Trie + some improvements

--- main.cpp
+++ main.cpp
@@ -17,17 +17,15 @@ int main() {
 //    cout << t.size() << endl;
 //    t.eraseRange(Data(1,1,1), Data(22,22,22));
 //    cout << t.size() << endl;
-    testBinarySearchTree();
-//    Trie tr;
-//    ifstream file("C:\\Users\\Victor\\Documents\\ADS\\Lab2_4\\words_alpha.txt");
-//    string str;
-//    while(getline(file,str, '\n'))
-//    {
-//        tr.insert(str);
-//    }
-//    file.close();
-////    tr.insert("abobus");
-////    tr.insert("abibus");
-//    tr.findByPrefix("objur");
+//    testBinarySearchTree();
+    Trie tr;
+    ifstream file("C:\\Users\\Victor\\Documents\\ADS\\Lab2_4\\words_alpha.txt");
+    string str;
+    while(getline(file,str, '\n'))
+    {
+        tr.insert(str);
+    }
+    file.close();
+    tr.findByPrefix("abobra");
     return 0;
 }
